24c9873bd4c92e672c29ba765fb4939f99ef90ab
LDF <liudengfeng_sd@outlook.com>
2024-08-01 03:53:45 +0800

    - vertexai

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
-const { VertexAI } = require('@google-cloud/vertexai');
+// const { VertexAI } = require('@google-cloud/vertexai');
 
 // const project = "enzh-428519";
 // const location = "asia-northeast1";
 // const model = "gemini-1.5-flash";
 
-const credentials = JSON.parse(process.env.REACT_APP_CREDENTIALS);
-const jwtInput = {
-    client_email: credentials.client_email,
-    private_key: credentials.private_key
-};
-const authOptions = {
-    credentials: jwtInput,
-    projectId: credentials.project_id,
-    scopes: ['https://www.googleapis.com/auth/cloud-platform']
-};
-const vertex_ai = new VertexAI({ googleAuthOptions: authOptions });
+// const credentials = JSON.parse(process.env.REACT_APP_CREDENTIALS);
+// const jwtInput = {
+//     client_email: credentials.client_email,
+//     private_key: credentials.private_key
+// };
+// const authOptions = {
+//     credentials: jwtInput,
+//     projectId: credentials.project_id,
+//     scopes: ['https://www.googleapis.com/auth/cloud-platform']
+// };
+// const vertex_ai = new VertexAI({ googleAuthOptions: authOptions });
 
 function Explore() {
     const [prompt, setPrompt] = useState('');
@@ -27,14 +27,15 @@ function Explore() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const request = {
-            contents: [
-                { role: "user", parts: [{ text: prompt }] }
-            ]
-        }
+        // const request = {
+        //     contents: [
+        //         { role: "user", parts: [{ text: prompt }] }
+        //     ]
+        // }
         try {
-            const result = await vertex_ai.generatedContent(request);
-            setGeneratedContent(JSON.stringify(result.response));
+            // const result = await vertex_ai.generatedContent(request);
+            const credentials = JSON.parse(process.env.REACT_APP_CREDENTIALS);
+            setGeneratedContent(JSON.stringify(credentials));
         } catch (error) {
             console.error('Error generating text:', error);
             setGeneratedContent('Error generating text.');
